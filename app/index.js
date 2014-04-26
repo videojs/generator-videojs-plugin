b8bac78952f8d4c6cfd25a75568d730985d07fb8
David LaPalomento <dlapalomento@gmail.com>
2014-04-26 19:16:05 -0400

    Try to look up the author name with git Call git config user.name and use that to set the default author name.

--- app/index.js
+++ app/index.js
@@ -4,6 +4,7 @@ var path = require('path');
 var yeoman = require('yeoman-generator');
 var chalk = require('chalk');
 var _ = require('underscore.string');
+var spawn = require('child_process').spawn;
 
 var VideojsPluginGenerator = yeoman.generators.Base.extend({
   init: function () {
@@ -17,7 +18,10 @@ var VideojsPluginGenerator = yeoman.generators.Base.extend({
   },
 
   askFor: function () {
-    var done = this.async();
+    var gitConfig,
+        author = '',
+        done = this.async(),
+        yo = this;
 
     // greet the user
     this.log(this.yeoman);
@@ -25,50 +29,61 @@ var VideojsPluginGenerator = yeoman.generators.Base.extend({
     // short generator description
     this.log(chalk.magenta('Build a video.js plugin with Grunt, npm, and qunit.'));
 
-    var prompts = [{
-      name: 'pluginName',
-      message: 'What would you like to call your plugin?',
-      default: 'videojs-' +
-        process.cwd().split(path.sep).slice(-1)[0]
-        .replace(/^videojs-/, '')
-        .replace(/^vjs-/, '')
-    }, {
-      name: 'description',
-      message: 'Describe your plugin in one short sentence:',
-      default: 'A revolutionary plugin for video.js'
-    },{
-      name: 'author',
-      message: 'What is your name?'
-    }, {
-      name: 'license',
-      message: 'What license is your plugin released under?',
-      default: 'Apache-2.0'
-    }];
+    // look up a default author name through git config
+    gitConfig = spawn('git', ['config', 'user.name']);
+    gitConfig.stdout.on('data', function(chunk) {
+      author += chunk;
+    });
+    gitConfig.on('close', function() {
+      var prompts = [{
+            name: 'pluginName',
+            message: 'What would you like to call your plugin?',
+            default: 'videojs-' +
+              process.cwd().split(path.sep).slice(-1)[0]
+              .replace(/^videojs-/, '')
+              .replace(/^vjs-/, '')
+          }, {
+            name: 'description',
+            message: 'Describe your plugin in one short sentence:',
+            default: 'A revolutionary plugin for video.js'
+          },{
+            name: 'author',
+            message: 'What is your name?'
+          }, {
+            name: 'license',
+            message: 'What license is your plugin released under?',
+            default: 'Apache-2.0'
+          }];
 
-    this.prompt(prompts, function (props) {
-      var now = new Date();
-      this.pluginName = props.pluginName;
-      this.camelPluginName =
-        _.camelize(this.pluginName.replace(/^videojs-/, ''));
-      this.humanPluginName =
-        _.titleize(_.humanize(this.pluginName.replace('videojs', 'video.js')));
-
-      this.author = props.author;
-      this.version = '0.1.0';
-      this.today =
-        now.getFullYear() + '-' + (now.getMonth() + 1) + '-' + (now.getDate());
-      this.year = now.getFullYear();
-
-      this.description = props.description;
-
-      if ((/Apache(-)?2(\.0)?/i).test(props.license)) {
-        this.license = 'Apache-2.0';
-      } else if ((/MIT/i).test(props.license)) {
-        this.license = 'MIT';
+      if (author !== '') {
+        prompts[2].default = _.trim(author);
       }
 
-      done();
-    }.bind(this));
+      yo.prompt(prompts, function (props) {
+        var now = new Date();
+
+        this.pluginName = props.pluginName;
+        this.camelPluginName =
+          _.camelize(this.pluginName.replace(/^videojs-/, ''));
+        this.humanPluginName =
+          _.titleize(_.humanize(this.pluginName.replace('videojs', 'video.js')));
+
+        this.author = props.author;
+        this.version = '0.1.0';
+        this.today =
+          now.getFullYear() + '-' + (now.getMonth() + 1) + '-' + (now.getDate());
+        this.year = now.getFullYear();
+
+        this.description = props.description;
+
+        if ((/Apache(-)?2(\.0)?/i).test(props.license)) {
+          this.license = 'Apache-2.0';
+        } else if ((/MIT/i).test(props.license)) {
+          this.license = 'MIT';
+        }
+        done();
+      }.bind(yo));
+    });
   },
 
   app: function () {
