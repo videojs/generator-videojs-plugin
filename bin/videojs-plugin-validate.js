4c93761d1eda033fa572bc4eb2ca369f77f8dbbc
Pat O'Neill <pgoneill@gmail.com>
2015-12-03 12:30:03 -0500

    Simpler output for validation

--- bin/videojs-plugin-validate.js
+++ bin/videojs-plugin-validate.js
@@ -1,11 +1,18 @@
 #!/usr/bin/env node
 
-var faucet = require('faucet');
+var chalk = require('chalk');
 var glob = require('glob');
 var path = require('path');
+var parser = require('tap-parser');
 var test = require('tape');
 
-test.createStream().pipe(faucet()).pipe(process.stdout);
+var tap = test.createStream().pipe(parser());
+
+tap.on('assert', function(assert) {
+  var color = assert.ok ? 'green' : 'red';
+  var prefix = assert.ok ? '✓' : '⨯';
+  process.stdout.write(chalk[color](prefix + ' ' + assert.name) + '\n');
+});
 
 glob(path.join(__dirname, '../es5-validate/*.test.js'), function(err, files) {
   if (err) {
