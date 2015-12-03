cbaff50c04b0333eb48c56f97bcfe2dbefeff053
Pat O'Neill <pgoneill@gmail.com>
2015-12-02 21:20:04 -0500

    Validate that core scripts exist

--- src/validate/package-json.test.js
+++ src/validate/package-json.test.js
@@ -17,12 +17,45 @@ test('package.json', t => {
     },
 
     '"description"'(tt) {
-      tt.ok(isNonEmptyString(pkg.description), 'package.json has a "description" property');
+      tt.ok(
+        isNonEmptyString(pkg.description),
+        'package.json has a "description" property'
+      );
       tt.end();
     },
 
     '"author"'(tt) {
-      tt.ok(!!pkg.author, 'package.json has an "author" property');
+      tt.ok(
+        isObject(pkg.author) || isNonEmptyString(pkg.author),
+        'package.json has an "author" property'
+      );
+      tt.end();
+    },
+
+    '"scripts"'(tt) {
+      tt.ok(isObject(pkg.scripts), 'package.json has an "scripts" object');
+      tt.end();
+    },
+
+    'core "scripts"'(tt) {
+      [
+        'build',
+        'build:js',
+        'build:test',
+        'clean',
+        'lint',
+        'start',
+        'test',
+        'watch',
+        'watch:js',
+        'watch:test',
+        'version'
+      ].forEach(script => {
+        tt.ok(
+          isNonEmptyString(pkg.scripts[script]),
+          `package.json "scripts" has a "${script}" script`
+        );
+      });
       tt.end();
     }
   });
