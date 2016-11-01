be9652860eb9b3c0a1051529f4fc636db80091d6
Pat O'Neill <pgoneill@gmail.com>
2016-11-01 15:17:37 -0400

    Updates for v3

--- bin/videojs-generator-cleanup.js
+++ bin/videojs-generator-cleanup.js
@@ -1,20 +1,29 @@
 #!/usr/bin/env node
 
-var chalk = require('chalk');
-var fs = require('fs-extra');
-var _ = require('lodash');
-var path = require('path');
-var semver = require('semver');
-var sh = require('shelljs');
+'use strict';
 
-var configs = {
+/**
+ * Performs cleanup after updating from one major version of the generator to
+ * another.
+ *
+ * @file bin/videojs-generator-cleanup.js
+ */
+const chalk = require('chalk');
+const fs = require('fs-extra');
+const _ = require('lodash');
+const path = require('path');
+const semver = require('semver');
+const sh = require('shelljs');
+const tsmlj = require('tsmlj');
+
+const configs = {
   v2: {
-    files: [
+    removals: [
       'dist-test',
       'test/karma',
       'scripts/npm-postversion-for-bower.sh',
       'scripts/npm-preversion-for-bower.sh',
-      'scripts/npm-version-for-bower.sh',
+      'scripts/npm-version-for-bower.sh'
     ],
     pkg: [
       'devDependencies.connect',
@@ -34,51 +43,140 @@ var configs = {
       'scripts.watch:js',
       'scripts.watch:test'
     ]
+  },
+  v3: {
+    removals: [
+      '.babelrc',
+      'dist',
+      'docs/api',
+      'es5',
+      'jsdoc.json',
+      'scripts',
+      'test/karma.conf.js',
+      'test/index.html',
+    ],
+    renames: {
+      'src/plugin.scss': 'src/css/index.scss',
+      'src/plugin.js': 'src/js/index.js',
+      'test/plugin.test.js': 'test/index.test.js'
+    },
+    pkg: [
+      'browserify',
+      'dependencies.browserify-versionify',
+      'dependencies.video.js',
+      'devDependencies.babel-cli',
+      'devDependencies.babel-plugin-transform-es3-member-expression-literals',
+      'devDependencies.babel-plugin-transform-es3-property-literals',
+      'devDependencies.babel-plugin-transform-object-assign',
+      'devDependencies.babel-preset-es2015',
+      'devDependencies.babelify',
+      'devDependencies.bannerize',
+      'devDependencies.bluebird',
+      'devDependencies.browserify',
+      'devDependencies.browserify-shim',
+      'devDependencies.bundle-collapser',
+      'devDependencies.budo',
+      'devDependencies.doctoc',
+      'devDependencies.glob',
+      'devDependencies.global',
+      'devDependencies.jsdoc',
+      'devDependencies.karma',
+      'devDependencies.karma-chrome-launcher',
+      'devDependencies.karma-detect-browsers',
+      'devDependencies.karma-firefox-launcher',
+      'devDependencies.karma-ie-launcher',
+      'devDependencies.karma-qunit',
+      'devDependencies.karma-safari-launcher',
+      'devDependencies.lodash',
+      'devDependencies.mkdirp',
+      'devDependencies.node-sass',
+      'devDependencies.semver',
+      'devDependencies.npm-run-all',
+      'devDependencies.qunitjs',
+      'devDependencies.rimraf',
+      'devDependencies.sinon',
+      'devDependencies.uglify-js',
+      'devDependencies.videojs-languages',
+      'devDependencies.videojs-standard',
+      'scripts.prebuild',
+      'scripts.build:css',
+      'scripts.build:css:bannerize',
+      'scripts.build:css:sass',
+      'scripts.build:js',
+      'scripts.build:js:babel',
+      'scripts.build:js:bannerize',
+      'scripts.build:js:browserify',
+      'scripts.build:js:collapse',
+      'scripts.build:js:uglify',
+      'scripts.build:lang',
+      'scripts.build:test',
+      'scripts.docs',
+      'scripts.docs:api',
+      'scripts.docs:toc',
+      'scripts.prepublish',
+      'scripts.pretest',
+      'scripts.test:chrome',
+      'scripts.test:firefox',
+      'scripts.test:ie',
+      'scripts.test:safari',
+      'scripts.version',
+      'style',
+      'videojs-plugin',
+      'vjsstandard'
+    ]
   }
 };
 
-var major = semver.major(require('../package.json').version);
-var config = configs['v' + major];
-var files, filename, pkg;
+const major = semver.major(require(path.join(__dirname, '..', 'package.json')).version);
+const config = configs[`v${major}`];
+
+const exists = (fname) => {
+  try {
+    fs.statSync(path.join(process.cwd(), fname));
+    return true;
+  } catch (x) {
+    return false;
+  }
+};
 
 if (!config) {
-  console.log(
-    chalk.yellow('There are no updates to be made from v%s.x.x to v%s.x.x'),
-    major ? major - 1 : 0,
-    major
-  );
-  process.exit();
+  console.log(chalk.yellow(tsmlj`
+    There are no updates to be made from
+    v${major ? major - 1 : 0}.x.x to v${major}.x.x
+  `));
+  process.exit(0);
 }
 
-if (Array.isArray(config.files) && config.files.length) {
-  files = config.files.filter(function(f) {
-    try {
-      fs.statSync(path.join(process.cwd(), f));
-      return true;
-    } catch (x) {
-      return false;
+if (config.removals.length) {
+  config.removals.filter(exists).forEach(fname => {
+    sh.rm('-rf', fname);
+    console.log(`Removed "${fname}" from this directory.`);
+  });
+}
+
+if (config.renames) {
+  Object.keys(config.renames).filter(exists).forEach(src => {
+    const dest = config.renames[src];
+
+    fs.move(src, dest, {clobber: true}, function(err) {
+      if (err) {
+        throw err;
+      }
+      console.log(`Renamed "${src}" to "${dest}.`);
+    });
+  });
+}
+
+if (config.pkg.length) {
+  const pkgPath = path.join(process.cwd(), 'package.json');
+  const pkg = require(pkgPath);
+
+  config.pkg.forEach(key => {
+    if (_.get(pkg, key)) {
+      _.set(pkg, key, undefined);
+      console.log(`Removed "${key}" from package.json.`);
     }
   });
 
-  sh.rm('-rf', files);
-
-  files.forEach(function(f) {
-    console.log('Removed "' + f + '" from this directory');
-  });
+  fs.writeJsonSync(pkgPath, pkg, {spaces: 2});
 }
-
-if (Array.isArray(config.pkg) && config.pkg.length) {
-  filename = path.join(process.cwd(), 'package.json');
-  pkg = require(filename);
-
-  config.pkg.forEach(function(p) {
-    if (_.get(pkg, p)) {
-      _.set(pkg, p, undefined);
-      console.log('Removed "' + p + '" from package.json');
-    }
-  });
-
-  fs.writeJsonSync(filename, pkg, {spaces: 2});
-}
-
-console.log(chalk.green('Done!'));
