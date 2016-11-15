2df6940848018f1d7bf264d4817cb004911acf84
Pat O'Neill <pgoneill@gmail.com>
2016-11-15 15:06:50 -0500

    More loopy

--- src/check/structure.test.js
+++ src/check/structure.test.js
@@ -2,17 +2,26 @@ import {isDir, isFile} from './lib';
 import tap from 'tap';
 
 // Required files/folders.
-tap.ok(isDir('src'), 'the "src/" directory exists');
-tap.ok(isDir('src/js/'), 'the "src/js/" directory exists');
-tap.ok(isFile('src/js/index.js'), 'the "src/js/index.js" file exists');
-tap.ok(isDir('test'), 'the "test/" directory exists');
-tap.ok(isFile('test/index.test.js'), 'the "test/index.test.js" file exists');
-tap.ok(isFile('.editorconfig'), 'the ".editorconfig" file exists');
-tap.ok(isFile('.gitignore'), 'the ".gitignore" file exists');
-tap.ok(isFile('.npmignore'), 'the ".npmignore" file exists');
-tap.ok(isFile('CHANGELOG.md'), 'the "CHANGELOG.md" file exists');
-tap.ok(isFile('index.html'), 'the "index.html" file exists');
-tap.ok(isFile('README.md'), 'the "README.md" file exists');
+[
+  'src',
+  'src/js',
+  'test'
+].forEach(f => {
+  tap.ok(isDir(f), `the "${f}/" directory exists`);
+});
+
+[
+  'src/js/index.js',
+  'test/index.test.js',
+  '.editorconfig',
+  '.gitignore',
+  '.npmignore',
+  'CHANGELOG.md',
+  'index.html',
+  'README.md'
+].forEach(f => {
+  tap.ok(isFile(f), `the "${f}" file exists`);
+});
 
 // Optional files/folders.
 const pkg = require(`${process.cwd()}/package.json`);
@@ -20,12 +29,9 @@ const pkg = require(`${process.cwd()}/package.json`);
 if (pkg.spellbook) {
 
   if (pkg.spellbook.css !== false) {
-    const cssFiles = ['index.css', 'index.scss', 'index.sass'];
-    const cssFile = cssFiles.find(fname => {
-      return isFile(`src/css/${fname}`);
-    });
+    const cssFiles = ['index.css', 'index.scss', 'index.sass'].map(f => `src/css/${f}`);
 
-    tap.ok(cssFile, `one of these files exists: ${cssFiles.join(',')}`);
+    tap.ok(cssFiles.find(isFile), `one of these files exists: "${cssFiles.join('", "')}"`);
   }
 
   if (pkg.spellbook.docs !== false) {
