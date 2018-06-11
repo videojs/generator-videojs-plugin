c2c30db306473655d195308cda20226e3b3303fa
brandonocasey <brandonocasey@gmail.com>
2018-06-11 16:17:04 -0400

    ignore: omit author in banner

--- generators/app/templates/scripts/_banner.js
+++ generators/app/templates/scripts/_banner.js
@@ -2,7 +2,6 @@ const pkg = require('../package.json');
 const bannerString = [
   `@name ${pkg.name}`,
   `@version ${pkg.version}`,
-  `@copyright ${pkg.author}`,
   `@license ${pkg.license}`
 ].join(' ');
 
