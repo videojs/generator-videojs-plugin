f7bb2af218a5625e6d86030566b2908fe47fbe62
Brandon Casey <BrandonOCasey@users.noreply.github.com>
2018-09-04 12:22:09 -0400

    fix: should not try to use jsdoc config (#226)

--- generators/app/package-json.js
+++ generators/app/package-json.js
@@ -229,7 +229,7 @@ const packageJSON = (current, context) => {
 
     _.assign(result.scripts, {
       'docs': 'npm-run-all docs:*',
-      'docs:api': 'jsdoc src -g plugins/markdown -r -c scripts/jsdoc.json -d docs/api',
+      'docs:api': 'jsdoc src -g plugins/markdown -r -d docs/api',
       'docs:toc': 'doctoc README.md'
     });
 
