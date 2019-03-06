34fc4980337f0969873cce21dad794388d0aa2a9
brandonocasey <brandonocasey@gmail.com>
2019-03-06 12:12:34 -0500

    ignore: add manual test script

---
+++ scripts/manual-test.js
@@ -0,0 +1,28 @@
+/* eslint-disable no-console */
+
+const helpers = require('yeoman-test');
+const libs = require('../test/libs.js');
+
+let tempDir;
+
+helpers.run(libs.GENERATOR_PATH)
+  .inTmpDir(function(dir) {
+    tempDir = dir;
+    console.log('Generating Project in a temp directory... ');
+  })
+  .withOptions(Object.assign(libs.options(), {skipInstall: false}))
+  .withPrompts({
+    name: 'manual-test',
+    author: 'John Doe',
+    description: 'wat is the plugin',
+    docs: true,
+    lang: true,
+    css: true,
+    prepush: false,
+    precommit: true
+  })
+  .then(function() {
+
+    console.log(`Generated Project in ${tempDir}`);
+    console.log('be sure to clean it up when your done!');
+  });
